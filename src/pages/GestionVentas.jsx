c925c52c632ce7382750d09cf0f1773a23513fa8
guidogacz <guidosistemas@gmail.com>
2021-10-26 21:00:36 -0500

    se cambia pagina de gestionventas, y estilogestionventas

--- src/pages/GestionVentas.jsx
+++ src/pages/GestionVentas.jsx
@@ -1,45 +1,49 @@
 import 'react-toastify/dist/ReactToastify.css';
+import 'styles/EstiloGestionVentas.css';
 import React, { useEffect, useState, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCheck, faUserEdit } from '@fortawesome/free-solid-svg-icons';
 import IconoVentas from 'media/IconoVentas.png';
 import { ToastContainer, toast } from 'react-toastify';
+import { nanoid } from 'nanoid';
+import axios from 'axios';
 
-const ventasBackend = [
-
-    {
-        iD: 1120,
-        total: 5000,
-        descripcion: 'garantìa,etc.',
-        fechaPago: "20-01-2022",
-        fechaFutura: "20-02-2022",
-        vendedor: 'Carla B',
-    },
-    {
-        iD: 1121,
-        total: 4000,
-        descripcion: 'garantìa,etc.',
-        fechaPago: "10-01-2022",
-        fechaFutura: "10-02-2022",
-        vendedor: 'Maria C',
-    },
-    {
-        iD: 1122,
-        total: 2000,
-        descripcion: 'garantìa,etc.',
-        fechaPago: "05-01-2022",
-        fechaFutura: "05-02-2022",
-        vendedor: 'Juan D',
-    },
-]
 
 const GestionVentas=()=>{
     const [mostrarTabla, setMostrarTabla] = useState(true);
     const [ventas, setVentas] = useState([]);
     const [textoBoton, setTextoBoton] = useState('Crear Nueva Venta');
+    const [consulta, setConsulta] = useState(true);
+
+    const getToken =()=>{
+        return `Bearer ${localStorage.getItem('token')}`;
+    }
 
     useEffect(() => {
-        setVentas(ventasBackend);
-      }, []);
+        const obtenerGestionVentas = async ()=>{
+            const options = {
+                method: 'GET',
+                url: 'https://git.heroku.com/still-shelf-57648.git/ventas/',
+                headers: {'Content-Type': 'application/json', Authorization: getToken()}
+              };
+             await axios.request(options).then(function (response) {
+                setVentas(response.data);
+              }).catch(function (error) {
+                console.error(error);
+              });
+        };
+        if (consulta){
+            obtenerGestionVentas();
+            setConsulta(false);
+        }
+    }, [consulta]);
 
+    useEffect(()=>{
+        if (mostrarTabla){
+            setConsulta(true);
+        }
+    },[mostrarTabla]);
+      
     useEffect(() => {
         if (mostrarTabla) {
           setTextoBoton('Crear Nueva Venta');
@@ -50,7 +54,7 @@ const GestionVentas=()=>{
 
     return(
         <div>
-            <h2 className='font-bold text-4xl text-center text-gray-800 my-2'>Administración de Ventas</h2>
+            <h2 className='font-bold text-4xl text-center text-yellow-400 my-2'>Administración de Ventas</h2>
             <div align='right'>
                 <button onClick={() => {setMostrarTabla(!mostrarTabla);}}
                         className={`text-white font-bold bg-green-500 p-5 rounded-full m-6 w-28 self-end`}>
@@ -58,12 +62,13 @@ const GestionVentas=()=>{
                 </button>
             </div>
             {mostrarTabla ? (
-            <TablaVentas listaVentas={ventas} />
+            <TablaVentas listaVentas={ventas} setConsulta={setConsulta} getToken={getToken} />
         ) : (
             <RegistroVentas
             setMostrarTabla={setMostrarTabla}
-	    listaVentas={ventas}
+	        listaVentas={ventas}
             setVentas={setVentas}
+            getToken={getToken}
             />
         )}
         <ToastContainer position='bottom-center' autoClose={5000} />     
@@ -73,7 +78,7 @@ const GestionVentas=()=>{
 }
 
 
-const RegistroVentas=({setMostrarTabla, listaVentas, setVentas})=>{
+const RegistroVentas=({setMostrarTabla, listaVentas, setVentas, getToken})=>{
 
     const form = useRef(null);
 
@@ -85,75 +90,122 @@ const RegistroVentas=({setMostrarTabla, listaVentas, setVentas})=>{
             regventas.forEach((value, key) => {
             nuevaVenta[key] = value;
             });
+
+            const options = {
+                method: 'POST',
+                url: 'https://git.heroku.com/still-shelf-57648.git/ventas/',
+                headers: { 'Content-Type': 'application/json', Authorization: getToken() },
+                data: { iD: nuevaVenta.iD, total: nuevaVenta.total, descripcion: nuevaVenta.descripcion, fechaPago: nuevaVenta.fechaPago, fechaFutura: nuevaVenta.fechaFutura, vendedor: nuevaVenta.vendedor},
+                };
+                  
+            await axios
+                .request(options)
+                .then(function (response) {
+                    console.log(response.data);
+                    toast.success('Venta añadida con éxito');
+                })
+                .catch(function (error) {
+                    console.error(error);
+                    toast.error('Error creando la venta');
+                });
+
+            setMostrarTabla(true)        
+            setVentas([...listaVentas, nuevaVenta]);              
         
-        setMostrarTabla(true)    
-        toast.success('Venta agregada con éxito');
-        setVentas([...listaVentas, nuevaVenta]);
     }
 
     return(
             <div className='flex flex-col items-center justify-center'>   
              <img src={IconoVentas} alt='imagen' className='h-40 w-auto my-7 rounded-full' />   
             <form ref={form} onSubmit={submitForm} className='flex flex-col'>
-                <h1 className='font-bold text-2xl text-blue-500 my-5 text-center'> Registro de Ventas</h1>
+                <h1 className='font-bold text-2xl text-green-500 my-5 text-center'> Registro de Ventas</h1>
                 <div className='flex flex-row'>
                 <label className='flex flex-col mx-5' htmlFor='iD'> 
-                    Ingrese su Identificador único de venta
+                    ID único de venta
                     <input 
                         name='iD' 
-                        className='bg-gray-50 border border-blue-600 p-2 rounded-lg my-2 w-50'
+                        className='bg-yellow-50 border border-green-600 p-2 rounded-lg my-2 w-50'
                         type='number' 
                         placeholder='1120' 
                         required/>
                 </label>
                 <label className='flex flex-col' htmlFor='total'> 
-                    Ingrese el valor total de la venta
+                Total de la venta
                     <input 
                         name='total' 
-                        className='bg-gray-50 border border-blue-600 p-2 rounded-lg my-2 w-50'
+                        className='bg-yellow-50 border border-green-600 p-2 rounded-lg my-2 w-50'
                         type='number' 
                         placeholder='5000' 
                         required/>
                 </label>
                 </div>
                 <div className='flex flex-row'>
-                <label className='flex flex-col mx-5' htmlFor='descripcion'> 
-                    Ingrese la descripción de la venta
+                <label className='flex flex-col mx-5' htmlFor='cantidad'> 
+                Cantidad de unidades
                     <input 
-                        name='descripcion' 
-                        className='bg-gray-50 border border-blue-600 p-2 rounded-lg my-2 w-50'
+                        name='cantidad' 
+                        className='bg-yellow-50 border border-green-600 p-2 rounded-lg my-2 w-50'
                         type='text' 
                         placeholder='Garantía, etc.' 
                         required/>
                 </label>
-                <label className='flex flex-col' htmlFor='fechaPago'> 
-                    Ingrese la fecha inicial de pago 
+                <label className='flex flex-col' htmlFor='preciounitario'> 
+                    Precio unitario 
                     <input 
-                        name='fechaPago' 
-                        className='bg-gray-50 border border-blue-600 p-2 rounded-lg my-2 w-50'
+                        name='preciounitario' 
+                        className='bg-yellow-50 border border-green-600 p-2 rounded-lg my-2 w-50'
                         type='text' 
-                        placeholder='20-01-2022' />
+                        placeholder='1000' />
                 </label>
-                <label className='flex flex-col' htmlFor='fechaFutura'> 
-                    Ingrese la fecha futura de pago 
+                <label className='flex flex-col' htmlFor='fechaventa'> 
+                    Fecha de venta 
                     <input 
-                        name='fechaFutura' 
-                        className='bg-gray-50 border border-blue-600 p-2 rounded-lg my-2 w-50'
-                        type='text' 
+                        name='fechaventa' 
+                        className='bg-yellow-50 border border-green-600 p-2 rounded-lg my-2 w-50'
+                        type='date' 
                         placeholder='20-02-2022' />
                 </label>
+                <label className='flex flex-col' htmlFor='identificacioncliente'> 
+                    Identificación cliente 
+                    <input 
+                        name='identificacioncliente' 
+                        className='bg-yellow-50 border border-green-600 p-2 rounded-lg my-2 w-50'
+                        type='number' 
+                        placeholder='1125635987' />
+                </label>
+                <label className='flex flex-col' htmlFor='nombrecliente'> 
+                    Nombre del cliente
+                    <input 
+                        name='nombrecliente' 
+                        className='bg-yellow-50 border border-green-600 p-2 rounded-lg my-2 w-50'
+                        type='text' 
+                        placeholder='Mario H' />
+                </label>
                 <label className='flex flex-col' htmlFor='vendedor'> 
-                    Ingrese el nombre del vendedor encargado de la venta
+                    Nombre del vendedor 
                     <input 
                         name='vendedor' 
-                        className='bg-gray-50 border border-blue-600 p-2 rounded-lg my-2 w-50'
+                        className='bg-yellow-50 border border-green-600 p-2 rounded-lg my-2 w-50'
                         type='text' 
                         placeholder='Maria C' />
+                </label>
+                <label className='flex flex-col' htmlFor='estadoventa'>
+                   Estado venta
+                    <select
+                        className='bg-gray-50 border border-green-600 p-2 rounded-lg my-2 w-50'
+                        name='estadoventa'
+                        defaultValue={0}>
+
+                        <option disabled value={0}>Elija una opción</option>
+                        <option>En proceso</option>
+                        <option>Entregada</option>
+                        <option>Cancelada</option>
+                    </select>
                 </label>
                 </div>
                                 
             <div className='my-4' align='center'>
-              <button type='submit'className='bg-blue-400 text-white font-bold rounded-full p-2'>Guardar Datos</button>
+              <button type='submit'className='bg-green-400 text-white font-bold rounded-full p-2'>Guardar Datos</button>
             </div>
         </form>
         </div>  
@@ -161,38 +213,150 @@ const RegistroVentas=({setMostrarTabla, listaVentas, setVentas})=>{
     )
 }
 
-const TablaVentas=({listaVentas})=>{
+const TablaVentas=({listaVentas, setConsulta, getToken})=>{
+
+    const [buscar, setBuscar] = useState('');
+    const [datoFiltrado, setDatoFiltrado] = useState(listaVentas)
+
+    useEffect(()=>{
+        setDatoFiltrado(
+            listaVentas.filter((elemento)=>{
+                return JSON.stringify(elemento).toLowerCase().includes(buscar.toLowerCase());
+            })
+        )
+    },[buscar, listaVentas]);
 
     return(
             <div className='flex flex-col items-center justify-center'>
-                <h1 className='font-bold text-4xl text-blue-500 my-5 text-center'> Listado de Ventas</h1>
-                <table className='border border-green-500 table-auto'>
+                <h1 className='font-bold text-4xl text-green-500 my-5 text-center'> Listado de Ventas</h1>
+                <input placeholder='Buscar'
+                value={buscar}
+                onChange={(e)=>setBuscar(e.target.value)}
+                className= 'border-2 border-green-700 px-3 py-1 rounded-md focus:outline-none focus:border-green-500'
+                />
+                < table className='tabla border border-green-500 table-auto'>
                     <thead>
                     <tr>
-                        <th className='border border-blue-600'>iD</th>
-                        <th className='border border-blue-600'>total</th>
-                        <th className='border border-blue-600'>descripcion</th>
-                        <th className='border border-blue-600'>fechaPago</th>
-                        <th className='border border-blue-600'>fechaFutura</th>
-                        <th className='border border-blue-600'>vendedor</th>
+                        <th className='border border-green-600'>ID</th>
+                        <th className='border border-green-600'>Total venta</th>
+                        <th className='border border-green-600'>Cantidad unidades producto</th>
+                        <th className='border border-green-600'>Precio producto</th>
+                        <th className='border border-green-600'>Fecha de Venta</th>
+                        <th className='border border-green-600'>ID cliente</th>
+                        <th className='border border-green-600'>Nombre cliente</th>
+                        <th className='border border-green-600'>Vendedor</th>
+                        <th className='border border-green-600'>Estado venta</th>
+                        <th className='border border-green-600'>Editar datos</th>
                     </tr>
                     </thead>
                     <tbody>
-                        {listaVentas.map((ventas) => {
+                        {datoFiltrado.map((ventas) => {
                         return (
-                        <tr>
-                            <td>{ventas.iD}</td>
-                            <td>{ventas.valor}</td>
-                            <td>{ventas.descripcion}</td>
-                            <td>{ventas.fechaPago}</td>
-                            <td>{ventas.fechaFutura}</td>
-                            <td>{ventas.vendedor}</td>
-                        </tr>
-                        );
+                            <FilaVentas key={nanoid()} ventas={ventas} setConsulta={setConsulta} getToken={getToken}/>
+                            );
                     })}
-                    </tbody>
+                        </tbody>
                 </table>
             </div>
+    )
+}
+
+const FilaVentas = ({ventas, setConsulta, getToken})=>{
+
+    const [editar, setEditar] = useState(false);
+    const [infoEditarVentas, setInfoEditarVentas] = useState({   
+
+        iD:ventas.iD, 
+        total:ventas.total,
+        cantidad:ventas.cantidad,
+        preciounitario:ventas.preciounitario, 
+        fechaventa:ventas.fechaventa, 
+        identificacioncliente:ventas.identificacioncliente,
+        nombrecliente:ventas.nombrecliente, 
+        vendedor:ventas.vendedor,
+        estadoventa:ventas.estadoventa, 
+        
+    })
+
+    const actualizarVentas = async ()=>{
+        const options = {
+            method: 'PATCH',
+            url: `https://git.heroku.com/still-shelf-57648.git/ventas/${ventas._id}`,
+            headers: {'Content-Type': 'application/json', Authorization: getToken()},
+            data: { ...infoEditarVentas },
+          };
+          
+          await axios.request(options).then(function (response) {
+            console.log(response.data);
+            toast.success('Venta modificada con éxito');
+            setConsulta(true);
+          }).catch(function (error) {
+            toast.error('Error modificando los datos de la venta');
+            console.error(error);
+          });
+
+    }
+
+    return(
+        <tr>
+        {editar ? (
+            <>
+            <td>
+                <input type='text' value={infoEditarVentas.iD} onChange={(e)=>setInfoEditarVentas({...infoEditarVentas, iD:e.target.value})} />
+            </td>
+            <td>
+                <input type='text' value={infoEditarVentas.total} onChange={(e)=>setInfoEditarVentas({...infoEditarVentas, total:e.target.value})}/>
+            </td>
+            <td>
+                <input type='text' value={infoEditarVentas.cantidad} onChange={(e)=>setInfoEditarVentas({...infoEditarVentas, cantidad:e.target.value})}/>
+            </td>
+            <td>
+                <input type='text' value={infoEditarVentas.preciounitario} onChange={(e)=>setInfoEditarVentas({...infoEditarVentas, preciounitario:e.target.value})}/>
+            </td>
+            <td>
+                <input type='text' value={infoEditarVentas.fechaventa} onChange={(e)=>setInfoEditarVentas({...infoEditarVentas, fechaventa:e.target.value})}/>
+            </td>
+            <td>
+                <input type='text' value={infoEditarVentas.identificacioncliente} onChange={(e)=>setInfoEditarVentas({...infoEditarVentas, identificacioncliente:e.target.value})}/>
+            </td>
+            <td>
+                <input type='text' value={infoEditarVentas.nombrecliente} onChange={(e)=>setInfoEditarVentas({...infoEditarVentas, nombrecliente:e.target.value})}/>
+            </td>           
+            <td>
+                <input type='text' value={infoEditarVentas.vendedor} onChange={(e)=>setInfoEditarVentas({...infoEditarVentas, vendedor:e.target.value})}/>
+            </td>
+            <td>
+            <select value={infoEditarVentas.estadoventa} onChange={(e)=>setInfoEditarVentas({...infoEditarVentas, estadoventa:e.target.value})} >
+                <option disabled value={0}>Elija una opción</option>
+                <option>En proceso</option>
+                <option>Entregada</option>
+                <option>Cancelada</option>
+            </select>
+            </td>
+            </>
+        ):(
+            <>
+            <td>{ventas.iD}</td>
+            <td>{ventas.total}</td>
+            <td>{ventas.cantidad}</td>
+            <td>{ventas.preciounitario}</td>
+            <td>{ventas.fechaventa}</td>
+            <td>{ventas.identificacioncliente}</td>
+            <td>{ventas.nombrecliente}</td>
+            <td>{ventas.vendedor}</td>
+            <td>{ventas.estadoventa}</td>             
+            </>
+        )}
+        <td>
+            <div className='flex items-center justify-center'>
+            {editar ? (
+                <FontAwesomeIcon onClick={()=>actualizarVentas() } icon={faUserCheck} className='text-green-400 hover:text-yellow-900' />
+            ):(
+                <FontAwesomeIcon onClick={()=>setEditar(!editar)} icon={faUserEdit} className='text-green-400 hover:text-yellow-900' />
+            )} 
+            </div>
+        </td>
+        </tr>       
     )
 }
 

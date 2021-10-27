0cd036a9cb95c7c0c7c345f0e7b3f36c52c590a3
guidogacz <guidosistemas@gmail.com>
2021-10-26 21:19:19 -0500

    se modifica pagina gestion productos

--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -1,4 +1,4 @@
-import React, {} from 'react';
+import React, {useState, useEffect} from 'react';
 import Sidebar from 'components/Sidebar';
 import SidebarResponsive from 'components/SidebarResponsive';
 import PrivateRoute from 'components/PrivateRoute';

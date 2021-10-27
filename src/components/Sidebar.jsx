7a906ac877a72aa838365a1befb08ca0c6af9da6
guidogacz <guidosistemas@gmail.com>
2021-10-26 21:52:06 -0500

    se cambia login, sidebar, privatelayout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,8 +5,65 @@ import ImagenSidebar from './ImagenSidebar';
 import { useAuth0 } from "@auth0/auth0-react";
 
 
+// const Sidebar = () => {
+//   const { user, logout } = useAuth0();
+
+//   const cerrarSesion=()=>{
+//     logout({ returnTo: window.location.origin })
+//     localStorage.setItem('token',null)
+//   }
+
+//   return (
+//     <nav className='hidden lg:flex lg:w-72 border border-gray-300 h-full flex-col bg-gray-100 p-4 sidebar'>
+//         {/* <Link to='/Login'> */}
+//           <ImagenSidebar />
+//         {/* </Link> */}
+
+//         <div>
+//           <Ruta  ruta='/GestionUsuarios' nombre='Perfil' usuario={user} />
+//         </div>
+
+//         <div className='my-4'>
+          
+//           <Ruta icono='' ruta='/GestionUsuarios' nombre='Usuarios' />
+//           <Ruta icono='' ruta='/GestionVentas' nombre='Ventas' />
+//           <Ruta icono='' ruta='/GestionProductos' nombre='Productos' />
+//           {/* <Ruta icono='' ruta='/Login' nombre='Login' /> */}
+//         </div>
+//         <button onClick={() => cerrarSesion() } className='bg-indigo-500 p-1 text-white rounded-md shadow-md hover:bg-red-400'>
+//         Cerrar Sesión
+//         </button>
+//     </nav>
+//   );
+// };
+
+// const Ruta = ({ icono, ruta, nombre, usuario }) => {
+//   const isActive = useActiveRoute(ruta);
+//   return (
+//     <Link to={ruta}>
+//       <button
+//         className={`p-1 my-2  bg-${
+//           isActive ? 'indigo' : 'gray'
+//         }-700 hover:bg-indigo-900 flex w-full items-center text-white rounded-md`}
+//       >
+//         {usuario ? (
+//           <>
+//           <img src={usuario.picture} className='h-5 w-5 rounded-full' />
+//             {usuario.name}</>
+//         ) : (
+//           <>
+//             <i className={`${icono} w-10`} />
+//              {nombre}
+//           </>
+//         )}
+                
+//       </button>
+//     </Link>
+//   );
+// };
+
 const Sidebar = () => {
-  const { user, logout } = useAuth0();
+  const { logout } = useAuth0();
 
   const cerrarSesion=()=>{
     logout({ returnTo: window.location.origin })
@@ -15,29 +72,23 @@ const Sidebar = () => {
 
   return (
     <nav className='hidden lg:flex lg:w-72 border border-gray-300 h-full flex-col bg-gray-100 p-4 sidebar'>
-        {/* <Link to='/Login'> */}
-          <ImagenSidebar />
-        {/* </Link> */}
+      <Link to='/Login'>
+        <ImagenSidebar />
+      </Link>
 
-        <div>
-          <Ruta  ruta='/GestionUsuarios' nombre='Perfil' usuario={user} />
-        </div>
-
-        <div className='my-4'>
-          
-          <Ruta icono='' ruta='/GestionUsuarios' nombre='Usuarios' />
-          <Ruta icono='' ruta='/GestionVentas' nombre='Ventas' />
-          <Ruta icono='' ruta='/GestionProductos' nombre='Productos' />
-          {/* <Ruta icono='' ruta='/Login' nombre='Login' /> */}
-        </div>
-        <button onClick={() => cerrarSesion() } className='bg-indigo-500 p-1 text-white rounded-md shadow-md hover:bg-red-400'>
-        Cerrar Sesión
-        </button>
+      <div className='my-4'>
+        <Ruta icono='' ruta='/GestionUsuarios' nombre='Usuarios' />
+        <Ruta icono='' ruta='/GestionVentas' nombre='Ventas' />
+        <Ruta icono='' ruta='/GestionProductos' nombre='Productos' />
+      </div>
+      <button onClick={() => cerrarSesion() } className='bg-indigo-500 p-1 text-white rounded-md shadow-md hover:bg-red-400'>
+       Cerrar Sesión
+      </button>
     </nav>
   );
 };
 
-const Ruta = ({ icono, ruta, nombre, usuario }) => {
+const Ruta = ({ icono, ruta, nombre }) => {
   const isActive = useActiveRoute(ruta);
   return (
     <Link to={ruta}>
@@ -46,17 +97,8 @@ const Ruta = ({ icono, ruta, nombre, usuario }) => {
           isActive ? 'indigo' : 'gray'
         }-700 hover:bg-indigo-900 flex w-full items-center text-white rounded-md`}
       >
-        {usuario ? (
-          <>
-          <img src={usuario.picture} className='h-5 w-5 rounded-full' />
-            {usuario.name}</>
-        ) : (
-          <>
-            <i className={`${icono} w-10`} />
-             {nombre}
-          </>
-        )}
-                
+        <icon className={`${icono} w-10`} />
+        {nombre}
       </button>
     </Link>
   );
